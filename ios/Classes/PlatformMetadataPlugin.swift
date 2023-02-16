51351bc0eadcb306c6e503c612fdc242128c2ac3
shivam mishra <mishra.shivam1311@gmail.com>
2023-02-16 12:54:07 +0530

    * fixed PlatformException for iOS

--- ios/Classes/PlatformMetadataPlugin.swift
+++ ios/Classes/PlatformMetadataPlugin.swift
@@ -9,6 +9,25 @@ public class PlatformMetadataPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+
+    switch call.method {
+            case "getMetaData":
+                let arg = call.arguments as! String
+                let apiURL = Bundle.infoPlistValue(forKey: arg) as? String
+                result(apiURL)
+
+            default:
+                result(nil)
+            }
   }
+
+}
+
+extension Bundle {
+    static func infoPlistValue(forKey key: String) -> Any? {
+        guard let value = Bundle.main.object(forInfoDictionaryKey: key) else {
+           return nil
+        }
+        return value
+    }
 }
