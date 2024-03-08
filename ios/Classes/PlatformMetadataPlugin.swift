5f951bb92f7aa791804ffc6d88f882f66970ee7e
Alfreedom <00tango.bromine@icloud.com>
2024-03-08 16:00:06 +0100

    make ios query more flexible

--- ios/Classes/PlatformMetadataPlugin.swift
+++ ios/Classes/PlatformMetadataPlugin.swift
@@ -2,25 +2,25 @@ import Flutter
 import UIKit
 
 public class PlatformMetadataPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "platform_metadata", binaryMessenger: registrar.messenger())
-    let instance = PlatformMetadataPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-
-    switch call.method {
-            case "getMetaData":
-                let arg = call.arguments as! String
-                let apiURL = Bundle.infoPlistValue(forKey: arg) as? String
-                result(apiURL)
-
-            default:
-                result(nil)
-            }
-  }
-
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "platform_metadata", binaryMessenger: registrar.messenger())
+        let instance = PlatformMetadataPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        
+        switch call.method {
+        case "getMetaData":
+            let arg = call.arguments as! String
+            let apiURL = Bundle.infoPlistValue(forKey: arg)
+            result(apiURL)
+            
+        default:
+            result(nil)
+        }
+    }
+    
 }
 
 extension Bundle {
